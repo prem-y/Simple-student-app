701a63a042649e75d9099c1edfb28ee917692f8a
Prem-rao <raopremwaghmare@gmail.com>
2023-09-05 18:13:54 +0530

    commit

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+//require express,mongoose,cors
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,50 @@
-
+import React from 'react'
 function App() {
   return (
     <>  
-      
+        <div className='container d-flex justify-content-center mt-4'>
+          <div className='rounded border border-dark'>
+            <form className='d-flex  p-2  mx-sm-3'>
+              <div className='me-2'>  
+                <input 
+                type="text"
+                placeholder='Name' className='form-control'/>
+              </div>
+              <div className='me-2'>
+                <input 
+                type="text"
+                placeholder='Roll no.' className='form-control'/>
+              </div>
+              <div className='form-group mx-sm-3 mb-2 d-flex'>
+                <button className='btn btn-outline-success me-2'>Find</button>
+                <button className='btn btn-outline-dark'>Add</button>
+              </div>
+            </form>
+            <div className='d-flex m-2'>
+              <table className='table table-dark table-bordered'>
+                <thead>
+                  <tr>
+                    <th scope='col'>Name</th>
+                    <th scope='col'>Roll no.</th>
+                    <th scope='col'>Operations</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Student name</td>
+                    <td>Student roll</td>
+                    <td>
+                      <div className='form-group mx-sm-3 mb-2 d-flex'>
+                      <button className='btn btn-success me-2'>Edit</button>
+                      <button className='btn btn-danger '>Delete</button>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
     </>
   );
 }
